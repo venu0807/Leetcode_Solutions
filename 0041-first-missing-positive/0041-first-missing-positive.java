29fb1fabe4d38a7166cdc6d8c56061e326eaee5c
Venu Gopal Reddy Palugulla <131956376+venu0807@users.noreply.github.com>
2025-09-26 11:30:52 +0530

    Time: 1 ms (100%), Space: 57.8 MB (20.3%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,17 +1,48 @@
+// class Solution {
+//     public int firstMissingPositive(int[] nums) {
+//         int value = 0;
+//         HashMap<Integer,Boolean> map = new HashMap<>();
+
+//         for(int i : nums){
+//             if(i > 0){
+//                 map.put(i, true);
+//             }
+//         }
+
+//         for(int i = 1; i<=nums.length;i++){
+//             if(!map.containsKey(i)) return i;
+//         }
+//         return nums.length +1 ;
+//     }
+// }
+
+
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        int value = 0;
-        HashMap<Integer,Boolean> map = new HashMap<>();
 
-        for(int i : nums){
-            if(i > 0){
-                map.put(i, true);
+        int len = nums.length;
+
+        for(int i =0 ; i<len; i++) {
+            if (nums[i] <=0 ) {
+                nums[i] = len + 1;
             }
         }
 
-        for(int i = 1; i<=nums.length;i++){
-            if(!map.containsKey(i)) return i;
+        for (int i = 0; i<len; i++) {
+            int value = Math.abs(nums[i]);
+            if (value <= len) {
+                nums[value-1] = -Math.abs(nums[value-1]);
+            }
         }
-        return nums.length +1 ;
+
+        for (int i = 0; i < len; i++) {
+            if (nums[i] > 0) {
+                return i + 1;
+            }
+        }
+
+        return len + 1;
     }
+
+
 }
