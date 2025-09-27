81cc0649a0fd6ec317a705a07c35e68198b08f03
Venu Gopal Reddy Palugulla <131956376+venu0807@users.noreply.github.com>
2025-09-27 21:51:30 +0530

    Time: 0 ms (100%), Space: 45.1 MB (44.81%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,18 +1,37 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character, Integer> mapm = new HashMap<>();
-
-        for (char c : magazine.toCharArray()) {
-            mapm.put(c, mapm.getOrDefault(c, 0) + 1);
-        }
-        
-        for (char c : ransomNote.toCharArray()) {
-            if (!mapm.containsKey(c) || mapm.get(c) == 0) {
+        //count array
+        int[] count = new int[26];
+        for( char c : ransomNote.toCharArray()){
+            //get index of this char in magazine
+            int i = magazine.indexOf(c,count[c-'a']);
+            if(i == -1){
                 return false;
             }
-            mapm.put(c, mapm.get(c) - 1);
+            count[c-'a'] = i+1 ;
         }
-
         return true;
     }
 }
+
+
+
+
+// class Solution {
+//     public boolean canConstruct(String ransomNote, String magazine) {
+//         HashMap<Character, Integer> mapm = new HashMap<>();
+
+//         for (char c : magazine.toCharArray()) {
+//             mapm.put(c, mapm.getOrDefault(c, 0) + 1);
+//         }
+
+//         for (char c : ransomNote.toCharArray()) {
+//             if (!mapm.containsKey(c) || mapm.get(c) == 0) {
+//                 return false;
+//             }
+//             mapm.put(c, mapm.get(c) - 1);
+//         }
+
+//         return true;
+//     }
+// }
