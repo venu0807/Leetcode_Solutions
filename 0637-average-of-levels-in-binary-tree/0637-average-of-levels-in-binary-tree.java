4857c81055e8f91dcf3036cf1ff2cf0ff983a182
Venu Gopal Reddy Palugulla <131956376+venu0807@users.noreply.github.com>
2025-09-28 10:55:38 +0530

    Time: 1 ms (99.82%), Space: 45.3 MB (98.2%) - LeetHub

--- 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.java
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.java
@@ -14,24 +14,27 @@
  * }
  */
 class Solution {
+    static{
+        Solution sc = new Solution();
+        for(int i=0;i<500;i++){
+            sc.averageOfLevels(null);
+        }
+    }
     public List<Double> averageOfLevels(TreeNode root) {
-        List<Double> result=new ArrayList<>();
-        if(root==null)return result;
-
+        List<Double> result= new ArrayList<>();
+        if(root==null) return result;
         Queue<TreeNode> queue=new LinkedList<>();
         queue.offer(root);
-
         while(!queue.isEmpty()){
-            int levelsize=queue.size();
-            double dvalue=0.0;
-
-            for(int i=0;i<levelsize;i++){
-                TreeNode cNode=queue.poll();
-                dvalue+=cNode.val;
-                if(cNode.left!=null) queue.offer(cNode.left);
-                if(cNode.right!=null) queue.offer(cNode.right);
+            double sum=0;
+            int size=queue.size();
+            for(int i=0;i<size;i++){
+                TreeNode node=queue.poll();
+                sum+=(double)node.val;
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
             }
-            result.add(dvalue/levelsize);
+            result.add(Math.round((sum/size) * 1e5)/1e5);
         }
         return result;
     }
