03d1059dd0d3eb117493ef7f81137dc99a93e2af
Venu Gopal Reddy Palugulla <131956376+venu0807@users.noreply.github.com>
2025-09-29 21:03:52 +0530

    Time: 33 ms (84.62%), Space: 58.3 MB (45.72%) - LeetHub

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,24 +1,58 @@
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
-        long max = 0, sum = 0;
-        int l = 0;
-
-        HashMap<Integer, Integer> freq = new HashMap<>();
-
-        for(int r=0;r<nums.length;r++){
-            sum += nums[r];
-            freq.put(nums[r], freq.getOrDefault(nums[r], 0) +1);
-
-            if(r - l + 1 == k){
-                if(freq.size() == k){
-                    max = Math.max(max,sum);
+        int n = nums.length;
+        Set<Integer> elements = new HashSet<>();
+        long currentSum = 0;
+        long maxSum = 0;
+        int begin = 0;
+        
+        for (int end = 0; end < n; end++) {
+            if (!elements.contains(nums[end])) {
+                currentSum += nums[end];
+                elements.add(nums[end]);
+                
+                if (end - begin + 1 == k) {
+                    maxSum = Math.max(maxSum, currentSum);
+                    currentSum -= nums[begin];
+                    elements.remove(nums[begin]);
+                    begin++;
                 }
-                sum -= nums[l];
-                freq.put(nums[l], freq.get(nums[l]) - 1);
-                if(freq.get(nums[l]) == 0) freq.remove(nums[l]);
-                l++;
+            } else {
+                while (nums[begin] != nums[end]) {
+                    currentSum -= nums[begin];
+                    elements.remove(nums[begin]);
+                    begin++;
+                }
+                begin++;
             }
         }
-        return max;
+        
+        return maxSum;
     }
 }
+
+
+// class Solution {
+//     public long maximumSubarraySum(int[] nums, int k) {
+//         long max = 0, sum = 0;
+//         int l = 0;
+
+//         HashMap<Integer, Integer> freq = new HashMap<>();
+
+//         for(int r=0;r<nums.length;r++){
+//             sum += nums[r];
+//             freq.put(nums[r], freq.getOrDefault(nums[r], 0) +1);
+
+//             if(r - l + 1 == k){
+//                 if(freq.size() == k){
+//                     max = Math.max(max,sum);
+//                 }
+//                 sum -= nums[l];
+//                 freq.put(nums[l], freq.get(nums[l]) - 1);
+//                 if(freq.get(nums[l]) == 0) freq.remove(nums[l]);
+//                 l++;
+//             }
+//         }
+//         return max;
+//     }
+// }
