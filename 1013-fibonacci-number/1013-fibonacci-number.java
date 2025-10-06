f6700c0e979aa3d989bacbe35078ff13331c205b
Venu Gopal Reddy Palugulla <131956376+venu0807@users.noreply.github.com>
2025-10-06 11:52:18 +0530

    Time: 0 ms (100%), Space: 40.7 MB (19.09%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,7 +1,21 @@
+// class Solution {
+//     public int fib(int n) {
+//         if(n==0) return 0;
+//         if(n==1) return 1;
+//         return fib(n-1) + fib(n-2);
+//     }
+// }
+
 class Solution {
     public int fib(int n) {
-        if(n==0) return 0;
-        if(n==1) return 1;
-        return fib(n-1) + fib(n-2);
+      if(n <= 1) return n;
+      int a = 0, b = 1;
+      for(int i = 2;i <= n; i++) {
+        int temp = b;
+        b = a + b;
+        a = temp;
+      }
+      return b;
+        
     }
 }
