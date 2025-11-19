2f2947ec1a853602b7c20bbee665618829e9364e
Venu Gopal Reddy Palugulla <131956376+venu0807@users.noreply.github.com>
2025-11-19 11:57:04 +0530

    Time: 4 ms (39.94%), Space: 45.3 MB (14.47%) - LeetHub

---
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int[] finalPrices(int[] prices) {
+        int n = prices.length;
+        int[] result = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && prices[stack.peek()] >= prices[i]) {
+                int index = stack.pop();
+                result[index] = prices[index] - prices[i];
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            int index = stack.pop();
+            result[index] = prices[index];
+        }
+
+        return result;
+    }
+}
