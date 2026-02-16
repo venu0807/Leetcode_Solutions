aa3d0121d9e00a668da8f05e6b9e741cc609898c
Venu Gopal Reddy Palugulla <131956376+venu0807@users.noreply.github.com>
2026-02-16 15:15:59 +0530

    Time: 1 ms (60.34%), Space: 42.7 MB (34.47%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int reverseBits(int n) {
+        int reverse = 0;
+        for(int i=1;i<= 32;i++){
+            reverse = reverse << 1;
+            reverse = reverse | (1 & n);
+            n = n >> 1;
+        }
+        return reverse;
+    }
+}
